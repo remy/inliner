4fb1190c414b30707a5d34893dce7025ec836acd
Build Server <devops@angelprivate.com>
2015-02-24 12:10:26 -0500

    Non-zero exit on failure, so that scripts using inliner know to halt

--- inliner.js
+++ inliner.js
@@ -311,6 +311,7 @@ function Inliner(url, options, callback) {
 
       } catch (e) {
         inliner.emit('error', 'Fatal error parsing HTML - exiting');
+        process.exit(1);
       }
     }
   });
