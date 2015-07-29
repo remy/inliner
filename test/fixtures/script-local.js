e20c0bd7bdc401510b1b50c0dfddc6d44199fc89
Hubert SABLONNIÈRE <hubert.sablonniere@gmail.com>
2015-07-29 03:05:06 +0200

    fix: inline scripts containing ETAGO properly
    The fix is done by escaping all "</script" to "<\/script" according to Mathias Bynens article :
    https://mathiasbynens.be/notes/etago

--- test/fixtures/script-local.js
+++ test/fixtures/script-local.js
@@ -3,3 +3,7 @@ var array = [4, 8, 15, 16, 23, 42];
 for (var i = 0; i < array.length; i++) {
     console.log(array[i]);
 }
+
+function surroundWithScriptTag(code) {
+    return '<script>' + code + '</script>';
+}
