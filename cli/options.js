b025f4b0b644b12446ea97b10bd5c95e07c806d9
Ricardo Pescuma Domenecci <ricardo@pescuma.org>
2015-11-02 15:58:18 -0200

    feat: add support for inlining .min js
    Closes #75

--- cli/options.js
+++ cli/options.js
@@ -10,8 +10,10 @@ function options(args) {
       'debug',
       'verbose',
       'noimages',
+      'nocompress',
       'nosvg',
       'videos',
+      'inlinemin',
     ],
     string: [ // options
       'encoding',
@@ -27,12 +29,14 @@ function options(args) {
       e: 'encoding',
       s: 'nosvg',
       o: 'videos',
+      m: 'inlinemin',
     },
   });
 
   // copy across specific options
   if (argv.nocompress) {
     argv.compressCSS = false;
+    argv.compressJS = false;
     argv.collapseWhitespace = false;
   }
   argv.images = !argv.noimages;
